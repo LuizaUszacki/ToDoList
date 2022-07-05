5b0cb9e54dfbf262f8948c27b109262967ee5615
Luiza Pereira Uszacki <luizapereirauszacki@gmail.com>
2022-07-05 17:00:46 -0300

    Mudança no tamanho do checkbox | Alinhamento do botão X | Adição das funcionalidades com Enter | Responsividade

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -1,32 +1,55 @@
 const $ = (x) => document.querySelector(x)
 
-
+//DOM Elements
 const btnAddTask = $("#btn-add-task")
 const fieldInputTask = $("#field-input-task")
 const ulListTasks = $("#ul-list-tasks")
-let numberTasks = 0
+//Variables
 let maxTasks = 100
 let tasks = []
 
+
+//Event Listeners
 window.addEventListener('keydown', pressedKey => {
     let wasEnterPressed = pressedKey.key === 'Enter'
-    if (wasEnterPressed && fieldInputTask === document.activeElement) {
-        addTestTask()
+    let hasInputFocus = document.activeElement === fieldInputTask
+    let elementWithFocus = document.activeElement
+    let hasCheckboxFocus = elementWithFocus.classList.contains('input-list') 
+
+    if (wasEnterPressed && hasInputFocus) {
+        addTask()
+    } else if (wasEnterPressed && hasCheckboxFocus) {
+        elementWithFocus.checked = !elementWithFocus.checked
     }
 })
+btnAddTask.addEventListener('click', addTask)
 
-btnAddTask.addEventListener('click', addTestTask)
+
+//Save in localStorage
+
+//Delete from localStorage
+
+//Press X to delete Task
+
+function saveLocalStorage(obj) {
+    //localStorage.setItem(`task${tasks.length}`)
+
+}
+
+function deleteLocalStorage() {
+    
+}
 
 
-function addTestTask() {
+//Functions
+function addTask() {
     try {
         if(!fieldInputTask.value) throw new Error('Você não digitou uma tarefa. Digite uma tarefa e tente novamente.')
         if(!fieldInputTask.value.trim()) throw new Error('Você não digitou uma tarefa. Digite uma tarefa e tente novamente.')
-        if(numberTasks >= maxTasks) throw new Error(`Você chegou ao limite de ${maxTasks} tarefas. Apague uma tarefa para adicionar outra.`)
-        addElement(fieldInputTask.value, ulListTasks)
+        if(tasks.length >= maxTasks) throw new Error(`Você chegou ao limite de ${maxTasks} tarefas. Apague uma tarefa para adicionar outra.`)
+        createNewElement(fieldInputTask.value, ulListTasks)
     }
     catch(e) {
-        console.log(e)
         swal({
             title:"Ops... Algo deu errado😥", 
             text: `${e.message}`,
@@ -38,17 +61,17 @@ function addTestTask() {
     }
 }
 
-
-function addElement(text, ulListTasks) {
+function createNewElement(text, ulListTasks) {
     let li = document.createElement('li')
     li.innerHTML = `
-        <input type="checkbox" class="input-list" id="input-id-${numberTasks}">
-        <label for="input-id-${numberTasks}" class="input-list">
+        <input type="checkbox" class="input-list" id="input-id-${tasks.length}">
+        <label for="input-id-${tasks.length}" class="input-list">
         ${text}
         </label>
-        <button class="input-list-button">
+        <button class="input-list-button" id="input-list-button-${tasks.length}">
         X
         </button>`
     ulListTasks.appendChild(li)
-    numberTasks++
+    tasks.push(li)
+    console.log(tasks)
 }
